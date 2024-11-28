55e30946712a33c4961fc24a2c7026f7453e20ef
tarkex0x <163349630+tarkex0x@users.noreply.github.com>
2024-11-28 15:51:40 +0200

    Added notification.go

---
+++ models/notification.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+type NotificationType int
+
+const (
+	EmailNotification NotificationType = iota + 1
+	SMSNotification
+	PushNotification
+)
+
+type Notification struct {
+	ID              string           
+	MessageContent  string           
+	Recipient       string           
+	NotificationType NotificationType 
+}
+
+func main() {
+	exampleNotification := Notification{
+		ID:              os.Getenv("NOTIFICATION_ID"),
+		MessageContent:  "Hello, your order has been shipped",
+		Recipient:       os.Getenv("NOTIFICATION_RECIPIENT"),
+		NotificationType: EmailNotification,
+	}
+
+	log.Printf("Sending notification: %+v\n", exampleNotification)
+}
